622420a0ea474cf436cc5c278d635d9672cc543e
Nick Kras Borges <35628546+nickkrasborges@users.noreply.github.com>
2019-12-18 09:54:47 -0300

    Create notificacoes.sh

---
+++ notificacoes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Olá, um novo pod acabou de ser criado!"}' https://hooks.slack.com/services/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
